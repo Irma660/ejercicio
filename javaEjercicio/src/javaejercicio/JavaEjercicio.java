46c1dc05af1a4d01213429df2b7e48d85ab54a25
irma660 <1325455@senati.pe>
2023-08-19 18:19:44 -0500

    añadiendo ejercicio

--- javaEjercicio/src/javaejercicio/JavaEjercicio.java
+++ javaEjercicio/src/javaejercicio/JavaEjercicio.java
@@ -3,5 +3,7 @@ package javaejercicio;
 public class JavaEjercicio {
 
     public static void main(String[] args) {
+        /*EJERCICIO: Que le pida al usuario ingresar 5 numeros al azar y que 
+        la aplicación lo ordene de menor a mayor*/
     }
 }
