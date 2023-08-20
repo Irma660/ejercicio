ada01a41ef2b4a7e36d25941381b66b52c29f576
irma660 <1325455@senati.pe>
2023-08-20 10:19:28 -0500

    importando scanner y array

--- javaEjercicio/src/javaejercicio/JavaEjercicio.java
+++ javaEjercicio/src/javaejercicio/JavaEjercicio.java
@@ -1,9 +1,12 @@
 package javaejercicio;
 /*@author IrmaZ*/
+    import java.util.Scanner;
+    import java.util.Arrays;
 public class JavaEjercicio {
-
     public static void main(String[] args) {
         /*EJERCICIO: Que le pida al usuario ingresar 5 numeros al azar y que 
         la aplicación lo ordene de menor a mayor*/
+        System.out.println("EJERCICIO NÚMERO UNO");
+        Scanner ingresar = new Scanner (System.in);
     }
 }
